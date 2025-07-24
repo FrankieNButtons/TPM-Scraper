a35fae4fb3f8cd3296a62e028c19d51443f6cddd
FrankieNButtons <1252077901@qq.com>
2025-07-24 18:44:55 +0800

    Add a little script

---
+++ generatePopulationList.py
@@ -0,0 +1,14 @@
+import os;
+
+
+def main():
+    for pop in ['CEU/', 'FIN/', 'GBR/', 'TSI/', 'YRI/']:
+        saveDir = "./Downloads/" + pop;
+        with open(f"{saveDir}/{pop.rstrip('/')}.txt", "w") as f:
+            for filename in os.listdir(saveDir):
+                f.write(filename.rstrip('.tsv') + "\n") if filename.endswith(".tsv") and not filename.endswith("M.tsv") else None;
+            print(f"{pop} done")    
+
+
+if __name__ == "__main__":
+    main();
